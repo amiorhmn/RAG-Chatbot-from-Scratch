52e35222a3d78d172b1237561267ffd6f1db65ca
Amio Rahman <amio.rhmn@gmail.com>
2025-03-18 10:02:36 +0600

    Add in-line comments

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ from utils import get_embeddings, retrieve, generate_response
 file_path = input('Please enter the path of your document: ')
 try:
     with open(file_path, encoding='utf-8') as file:
-        doc = file.readlines()
+        doc = file.readlines() # doc is a list of strings where every element is a line from the text file
         print(f'Document loaded with {len(doc)} entries.')
 except Exception:
-    raise
+    raise # raise exception if no file is found in the path or the file is of invalid format
 
-doc_embedding = get_embeddings(doc)
+doc_embedding = get_embeddings(doc) # the embedding matrix
 try:
     while True:
         input_query = input('Ask a question: ')
-        if input_query in ['quit', 'exit']:
+        if input_query in ['quit', 'exit']: # keywords to trigger exit from the application
             break
-        retrieved_knowledge = retrieve(input_query, doc, doc_embedding)
-        output_response = generate_response(input_query, retrieved_knowledge)
+        retrieved_knowledge = retrieve(input_query, doc, doc_embedding) # list of relevant texts from the document
+        output_response = generate_response(input_query, retrieved_knowledge) # the answer string of the LLM response
         print('Chatbot: ' + output_response)
 except KeyboardInterrupt:
-    print(f'\nExiting due to KeyboardInterrupt.')
+    print(f'\nExiting due to KeyboardInterrupt.') # exit if pressed ctrl+c
 
 if __name__ == '__main__':
     pass
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-
+# Generate embedding matrix from list of strings
 def get_embeddings(texts: list[str]):
     url = 'http://localhost:11434/api/embed'
     data = {
@@ -15,22 +15,22 @@ def get_embeddings(texts: list[str]):
     return response['embeddings']
 
 
-
+# Retrieve the relevant strings from the stored document
 def retrieve(query: str, doc: list[str], doc_embedding):
     query_embedding = get_embeddings([query])
-    similarities = cosine_similarity(query_embedding, doc_embedding)
-    indexed_similarities = list(enumerate(similarities[0]))
-    indexed_similarities.sort(key=lambda x: x[1], reverse=True)
+    similarities = cosine_similarity(query_embedding, doc_embedding) # list[list[float]]
+    indexed_similarities = list(enumerate(similarities[0])) # Create a list of tuples like (index, score)
+    indexed_similarities.sort(key=lambda x: x[1], reverse=True) # Sort the list by descending order of the scores
 
     similar_texts = []
     for i, score in indexed_similarities:
-        if score > 0.6:
+        if score > 0.6: # filter the chunks that have similarity score > 0.6
             similar_texts.append(doc[i])
 
     return similar_texts
 
 
-
+# Generate response from LLM
 def generate_response(query: str, retrieved_texts: list[str]):
     
     if retrieved_texts == []:
